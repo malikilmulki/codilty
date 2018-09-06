69a071b1bfe2af54f3e53fc86e01820c2960e23b
Mulki <malikil.mulki@bizintek.com>
2018-09-06 13:55:55 +0700

    initial commit
    Initial commit

---
+++ Codility/Lesson 1/BinaryGap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lesson_1
+{
+    public class BinaryGap
+    {
+        public BinaryGap()
+        { }
+
+        public int Solution(int N)
+        {
+            String binary = Convert.ToString(N, 2);
+
+            int lastPosition = binary.Length - 1;
+
+            while (binary[lastPosition] == '0')
+            {
+                lastPosition--;
+            }
+
+            int counter = 0;
+            int maxGap = 0;
+
+            for (; lastPosition >= 0; lastPosition--)
+            {
+                if (binary[lastPosition] == '1')
+                {
+
+                    if (counter > maxGap)
+                    {
+                        maxGap = counter;
+                    }
+
+                    counter = 0;
+                }
+                else {
+                    counter++;
+                }
+            }
+
+            return maxGap;
+        }
+    }
+}
---
+++ Codility/Program.cs
@@ -0,0 +1,24 @@
+﻿using Codility.Lesson_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lesson 1, Binary Gap
+            BinaryGap gap = new BinaryGap();
+            gap.Solution(1041);
+            gap.Solution(15);
+            gap.Solution(32);
+
+            
+
+        }
+    }
+}
